e7c3b527e3c7d5314e549be3450bac19c4642343
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-28 10:22:10 +0100

    fixed

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -3,5 +3,16 @@ package school.mjc.stage0.module3.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         //write code here
+        int one;
+        int two;
+        int three;
+
+        one = 1;
+        two = 10;
+        three = 100;
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
     }
 }
